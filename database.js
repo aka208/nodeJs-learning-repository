f8c16a8a80dc05213f5825eaed42ee932ad5fd38
Akash Singh <2081993akash@gmail.com>
2026-05-30 15:01:07 +0530

    MongoDB integration in database.js file

---
+++ database.js
@@ -0,0 +1,61 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const dns = require("dns");
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+// Uy29TKVhvcT4PJqE;
+const URL =
+  "mongodb+srv://akashbtw_db_user:Uy29TKVhvcT4PJqE@cluster0.gcwleop.mongodb.net/";
+
+// const client = new MongoClient(URL, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   },
+//   autoSelectFamily: false, // Explicitly disable auto selection of IP family
+// });
+const client = new MongoClient(URL);
+const dbName = "HelloWorld";
+
+async function main() {
+  await client.connect();
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  const collection = db.collection("User");
+  const data = {
+    firstName: "Akshay",
+    lastName: "Saini",
+    city: "Delhi",
+    phone: "1234567890",
+  };
+  const data2 = {
+    firstName: "Amar",
+    lastName: "Makhija",
+    city: "Ujjain",
+    phone: "2345678909",
+  };
+
+  // insert many data into collection
+  const insertResult = await collection.insertMany([data]);
+  console.log("Inserted documents =>", insertResult);
+
+  // insert one data into collection
+  const insertOneResult = await collection.insertOne(data2);
+  console.log("Inserted one document =>", insertOneResult);
+
+  // read data
+  const findResult = await collection.find({}).toArray();
+  console.log("Found documents =>", findResult);
+
+  // count documents
+  const count = await collection.countDocuments({});
+  console.log("Total documents in collection =>", count);
+
+  return "done.";
+}
+
+main()
+  .then(console.log)
+  .catch(console.error)
+  .finally(() => client.close());
